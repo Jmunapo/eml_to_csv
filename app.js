6d71b8a5a1a655cfd608c6aaffeb8bcffe6b7da8
Jmunapo <jmunapo@gmail.com>
2019-09-24 10:58:37 +0200

    V1 completed

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const fs = require("fs");
 const emlformat = require("eml-format");
+const emailsInString = require("emails-in-string");
+const converter = require("convert-array-to-csv");
 
 const EMAILS = "./emails/";
 
+let emails_arr = [];
 fs.readdir(EMAILS, (err, emails) => {
   if (err) return console.log(err);
   emails.forEach(email => {
     let eml = fs.readFileSync(`./emails/${email}`, "utf-8");
     emlformat.read(eml, (e, data) => {
       if (e) return console.log(e);
-      //   fs.writeFileSync("sample.json", JSON.stringify(data, " ", 2));
-      console.log(data.text);
+      let addrs = emailsInString(data.text);
+      emails_arr = [...emails_arr, ...addrs];
     });
   });
+  let em_obj = [];
+  emails_arr.forEach((em, i) => {
+    em_obj.push({
+      user: i + 1,
+      email: em
+    });
+  });
+
+  const toCsv = converter.convertArrayToCSV(em_obj);
+  fs.writeFileSync("emails.csv", toCsv);
+  //   console.log(toCsv);
 });
